4b8056dd1e1af5964c11cf379bd0508d30129fe0
Anthony Duque <duquea95@gmail.com>
2018-05-23 05:39:42 -0400

    add

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,8 @@
 $(function(){
+    $menu = $("div.burger");
+    $nav = $("nav");
+
+    // Get the navbar
     $(window).scroll(function(){
         if ($(window).scrollTop() > 150){
             $('nav').addClass('affix');
@@ -7,4 +11,32 @@ $(function(){
             $('nav').removeClass('affix');
         }
     });
+
+    $menu.on('click',function(){
+        if($menu.hasClass("change")){
+            $menu.removeClass("change");
+            closeNav($nav);
+        }
+        else{
+            console.log("Hello world!");
+            openNav($nav);
+            $menu.addClass("change");
+        }
+
+        /* Open */
+        function openNav(){
+            console.log("Open Nav");
+            $nav.css("height", "100%");
+            $(".bar").css("background-color", "rgb(255,255,255)");
+            $('ul.main-nav').css("display", "block");
+            $nav.css("display", "block");
+        }
+    });
+
+    /* Close */
+    function closeNav() {
+        $($nav).css("height", "10%");
+        $('ul.main-nav').css("display", "none");
+    }
+
 });
